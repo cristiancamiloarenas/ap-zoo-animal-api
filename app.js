d9f37d7c011a5618f0e176ac1adbf25d3b12650f
Cristian Camilo Arenas Castellanos <44928834+cristiancamiloarenas@users.noreply.github.com>
2022-05-28 19:35:44 -0500

    Implemented REST API connected with Amazon RDS MySQL

--- app.js
+++ app.js
@@ -3,7 +3,8 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-
+let cors = require('cors');
+app.use(cors());
 var indexRouter = require('./routes/index');
 
 var app = express();
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,79 @@
 var express = require('express');
 var router = express.Router();
+var mysql = require('mysql2');
+const bodyParser = require('body-parser');
+router.use(bodyParser.json());
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+var connection = mysql.createConnection({
+    host: "mysql-database.c841feom24hb.us-east-1.rds.amazonaws.com",
+    user: "admin",
+    password: "adminadmin",
+    port: "3306",
+    database: "schema"
+});
+connection.connect(function (err) {
+    if (err) {
+        console.error('Database connection failed: ' + err.stack);
+        return;
+    }
+    console.log('Connected to database.');
+    connection.query('CREATE DATABASE IF NOT EXISTS main;');
+    connection.query('USE main;');
+    connection.query('CREATE TABLE IF NOT EXISTS Animal(id varchar (200), nombre varchar(200), sexo varchar(30), especie varchar(200));', function(error, result, fields) {
+        console.log(result);
+    });
+
 });
 
+/* GET home page. */
+router.get('/readAnimals', (req, res) => {
+    var sql = "SELECT * FROM main.Animal";
+    connection.query(sql,(err,result)=>{
+        if(err) throw err;
+        console.log(result);
+        res.statusCode=200;
+        res.setHeader('Content-Type',"application/json");
+        res.json({
+            error:err,
+            rows: result
+        })
+
+    })
+})
+
+router.post("/createAnimal", (req, res) => {
+    var sql = `INSERT INTO main.Animal VALUES('${req.body.id}','${req.body.nombre}','${req.body.sexo}','${req.body.especie}')`;
+    connection.query(sql,(err,result)=>{
+        if(err) throw err;
+        res.statusCode=200;
+        res.setHeader('Content-Type',"application/json");
+        res.json({
+            error:err
+        })
+    })
+})
+
+router.put("/updateAnimal", (req, res) => {
+    var sql = `UPDATE main.Animal SET nombre='${req.body.nombre}',sexo = '${req.body.sexo}', especie='${req.body.especie}' where id='${req.body.id}'`;
+    connection.query(sql,(err,result)=>{
+        if(err) throw err;
+        res.statusCode=200;
+        res.setHeader('Content-Type',"application/json");
+        res.json({
+            error:err
+        })
+    })
+})
+
+router.delete("/deleteAnimal", (req, res) => {
+    var sql = `DELETE FROM main.Animal WHERE id='${req.body.id}'`;
+    connection.query(sql,(err,result)=>{
+        if(err) throw err;
+        res.statusCode=200;
+        res.setHeader('Content-Type',"application/json");
+        res.json({
+            error:err
+        })
+    })
+})
 module.exports = router;
